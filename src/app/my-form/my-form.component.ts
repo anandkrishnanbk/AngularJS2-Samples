cc82cdd62061af66cbd70e9f39e40a9f6ba7e2be
anand.krishnankutty@bcbsnc.com <anand@anand-mbp.bcbsnc.com>
2017-08-04 14:26:51 -0400

    addedfiles

--- src/app/my-form/my-form.component.ts
+++ src/app/my-form/my-form.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-my-form',
   template: `
+    {{message}}
    <input #myInput type="text" />
    <button (mouseover)="onClick($event,myInput.value)">Click button</button>
   `,
-  styles: []
+  styles: [`:host{
+    display: flex;
+    flex-direction: column;
+  }
+    
+    input:focus{
+      font-weight: bold;
+      outline:none
+    }
+    input{
+      margin-bottom: 5px;
+    }
+    button{
+      border:none;
+      background-color: green;
+      color:white;
+    }
+    *{
+      font-family: Helvetica;
+    }
+  
+  `]
 })
 export class MyFormComponent implements OnInit {
-
+ @Input() message;
   constructor() {
    }
   onClick(event,value)
